3685576c01642aab22862cf9be56425680a8d70f
rlaehgus777 <joker112960@gmail.com>
2026-08-19 23:32:36 +0900

    260819

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper('.every_wrap', {
+    direction: 'vertical',   // 핵심: 가로 → 세로
+    slidesPerView: 1,
+    mousewheel: true,        // 마우스 휠로 다음 슬라이드 넘기고 싶으면
+    speed: 600,
+});
