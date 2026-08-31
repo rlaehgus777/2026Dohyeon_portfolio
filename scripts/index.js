1a04d9470b38a50c2a89f9fe208529d9565e7be2
rlaehgus777 <joker112960@gmail.com>
2026-08-31 17:10:21 +0900

    프레임제작 완료

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 const swiper = new Swiper('.every_wrap', {
     direction: 'vertical',   // 핵심: 가로 → 세로
-    slidesPerView: 1,
-    mousewheel: true,        // 마우스 휠로 다음 슬라이드 넘기고 싶으면
+    slidesPerView: 1,        // 마우스 휠로 다음 슬라이드 넘기고 싶으면
+    mousewheel : true,
     speed: 600,
+});
+document.querySelectorAll('.contents').forEach(link => {
+    link.addEventListener('click', function(e) {
+    const slideIndex = this.getAttribute('data-slide'); // this = .contents 링크
+    swiper.slideTo(parseInt(slideIndex));
+    });
 });
